85479f2e92f83fdac1efc7e3178367e130422de3
TomAre86 <tommy_m86@hotmail.com>
2023-08-23 11:13:29 +0200

    Added fargedivs

--- SideTo/side2.js
+++ SideTo/side2.js
@@ -3,10 +3,16 @@ toggleList();
 function toggleList() {
 
     if (list == false) {
-        document.getElementById("divList").innerHTML = /*HTML*/`<ul>
-        <li><a href="https://TM-sha.github.io/GetAcademy/OppgaveParametre"
-                title="Gå til"><code>Funksjon & Parametre</code></a></li>
-    </ul> `;
+        document.getElementById("divList").innerHTML = /*HTML*/`
+        <ul>
+        <li>
+            <a href="https://TM-sha.github.io/GetAcademy/OppgaveParametre"
+                title="Gå til"><code>Funksjon & Parametere</code></a>
+        </li><br>
+        <li>
+            <a href="https://TM-sha.github.io/GetAcademy/FargeDivs" title="Gå til"><code>Fargediv's</code></a>
+        </li>
+    </ul> `
         list = true;
     } else if (list == true) {
         document.getElementById("divList").innerHTML = '';
