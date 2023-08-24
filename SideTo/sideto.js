af54a60bc1b5613485741b16b128e205046ac2af
TomAre86 <tommy_m86@hotmail.com>
2023-08-24 18:24:07 +0200

    Path update
    Renaming

--- SideTo/sideto.js
+++ SideTo/sideto.js
@@ -9,23 +9,29 @@ function toggleList() {
             <a href="https://TM-sha.github.io/GetAcademy/OppgaveParametre"
                 title="Gå til"><code>Funksjon & Parametere</code></a>
         </li><br>
+
         <li>
             <a href="https://TM-sha.github.io/GetAcademy/FargeDivs" title="Gå til"><code>Fargediv's</code></a>
         </li><br>
+
         <li>
             <a href="https://TM-sha.github.io/GetAcademy/StopWatch" title="Gå til"><code>StopWatch</code></a>
         </li><br>
+
         <li>
             <a href="https://TM-sha.github.io/GetAcademy/Trafikklys" title="Gå til"><code>Trafikklys</code></a>
         </li><br>
+
         <li>
             <a href="https://TM-sha.github.io/GetAcademy/Tastatur" title="Gå til"><code>Tastatur</code></a>
         </li><br>
+
         <li>
             <a href="https://TM-sha.github.io/GetAcademy/Lightbulb" title="Gå til"><code>Lightbulb</code></a>
         </li><br>
+        
         <li>
-            <a href="https://TM-sha.github.io/GetAcademy/Return" title="Gå til"><code>Return</code></a>
+            <a href="https://TM-sha.github.io/GetAcademy../Return" title="Gå til"><code>Return</code></a>
         </li>
     </ul> `
         list = true;
