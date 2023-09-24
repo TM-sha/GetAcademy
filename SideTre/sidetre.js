d8911bd4f7359b40faa45b260140d060222d976e
Tom86 <tommy_m86@hotmail.com>
2023-09-24 16:41:24 +0200

    Added Objekter

--- SideTre/sidetre.js
+++ SideTre/sidetre.js
@@ -21,7 +21,13 @@ function toggleList() {
                 <a href="https://TM-sha.github.io/GetAcademy/TallGenererer" title="Gå til"><code>Tall-genererer</code></a>
             </li><br>
             <li>
-                <a href="https://TM-sha.github.io/GetAcademy/EightBall" title="Gå til"><code>8 ball</code></a><br><br>
+                <a href="https://TM-sha.github.io/GetAcademy/HandleVogn" title="Gå til"><code>Handlevogn</code></a>
+            </li><br>
+            <li>
+                <a href="https://TM-sha.github.io/GetAcademy/EightBall" title="Gå til"><code>8 ball</code></a>
+            </li><br>
+            <li>
+                <a href="https://TM-sha.github.io/GetAcademy/Objekter" title="Gå til"><code>Objekter (Oppgave tabell)</code></a><br><br>
                 <a href="https://TM-sha.github.io/GetAcademy/SideTo/" title="Forrige side" style= "color:orange; margin-left:-20px">Forrige</a>
             </li>
         </ul> `;
